b98a279ed69496e15124c74ca43e9cb0ab486e71
ggs20 <ggs20@DESKTOP-VTMB6BE>
2020-03-24 10:57:09 +0100

    Prueba de GitHub

---
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,14 @@
+
+package pruebagithub;
+
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+
+        Persona persona1=new Persona("Pepe","López");
+        
+        System.out.println(persona1+"\n");
+        
+    }
+    
+}
